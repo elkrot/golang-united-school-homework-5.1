fe99bd75c4965b1634919ab55aa6fc9a7363fb84
Kolodiazhnyi Serhii <elkrot@gmail.com>
2022-05-20 10:08:17 +0300

    Update square.go

--- square.go
+++ square.go
@@ -10,7 +10,7 @@ type Square struct {
 }
 
 func (s *Square) End() Point {
-	return Point{uint(s.start.x) + s.a, uint(s.start.y) - s.a}
+	return Point{s.start.x + int(s.a), s.start.y - int(s.a)}
 }
 
 func (s *Square) Area() uint {
