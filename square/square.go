ee94a2d4141e549a09b1fceabb80acd2f04e50af
Kolodiazhnyi Serhii <elkrot@gmail.com>
2022-05-20 09:46:22 +0300

    Create square.go

---
+++ square/square.go
@@ -0,0 +1,22 @@
+package square
+
+type Point struct {
+	X, Y uint
+}
+
+type Square struct {
+	Start Point
+	A     uint
+}
+
+func (s *Square) End() Point {
+	return Point{s.Start.X + s.A, s.Start.Y - s.A}
+}
+
+func (s *Square) Area() uint {
+	return s.A * s.A
+}
+
+func (s *Square) Perimeter() uint {
+	return s.A * 4
+}
